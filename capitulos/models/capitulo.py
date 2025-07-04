377bbecdc0a4b13bd00eb698993ed4e5af2ceb3d
sergiodeveloper5 <156307738+sergiodeveloper5@users.noreply.github.com>
2025-07-04 11:02:16 +0200

    Si

--- capitulos/models/capitulo.py
+++ capitulos/models/capitulo.py
@@ -7,7 +7,6 @@ class CapituloContrato(models.Model):
     _description = 'Capítulo de Contrato'
 
     name = fields.Char(string='Nombre del Capítulo', required=True)
-    codigo = fields.Char(string='Código', required=True)
     description = fields.Text(string='Descripción')
     componente_ids = fields.One2many('capitulo.componente', 'capitulo_id', string='Componentes')
     condiciones_legales = fields.Text(string='Condiciones Legales')
